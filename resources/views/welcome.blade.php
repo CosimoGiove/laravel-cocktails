89bd4348edc22486d9e441430906a64f8d34337c
cosimogiove <cosimo18497@gmail.com>
2024-01-31 15:47:06 +0100

    feat:titolo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,13 +5,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Laravel Template</title>
+    <title>Db_cocktails</title>
     @vite('resources/js/app.js')
 </head>
 
 <body>
     <main>
-        ciao
     </main>
 </body>
 

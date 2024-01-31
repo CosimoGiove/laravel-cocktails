04efdf7cffc10886c9e7a06637ef27bb5ac2acde
cosimogiove <cosimo18497@gmail.com>
2024-01-31 17:31:18 +0100

    feat: terminato

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -18,47 +18,51 @@
         </div>
     </header>
     <main>
-        <div class="d-flex container">
+        <div class="container d-flex">
             <div class="row">
                 <ul>
                     @foreach ($cocktails as $cocktail)
                         <li>
                             <h2>
-                                {{$cocktail["name"]}}
+                                {{ $cocktail['name'] }}
                             </h2>
                             <p>
-                                Main Alcohol: 
-                                {{$cocktail["main_alcohol"]}}
+                                Main Alcohol:
+                                {{ $cocktail['main_alcohol'] }}
                             </p>
                             <p>
-                               Ingredients: 
-                                {{$cocktail["ingredients"]}}
+                                Ingredients:
+                                {{ $cocktail['ingredients'] }}
                             </p>
                             <p>
-                                Preparation: 
-                                 {{$cocktail["preparations"]}}
-                             </p>
-                             <p>
-                                Glass Type: 
-                                 {{$cocktail["glass_type"]}}
-                             </p>
-                             <p>
-                                Serving Temperature:   
-                                 {{$cocktail["serving_temperature"]}}
-                             </p>
-                             <p>
-                            Garnish: 
-                                 {{$cocktail["garnish"]}}
-                             </p>
-                             <p>
-                               price: 
-                                 {{$cocktail["price"]}}€
-                             </p>
+                                Preparation:
+                                {{ $cocktail['preparations'] }}
+                            </p>
+                            <p>
+                                Glass Type:
+                                {{ $cocktail['glass_type'] }}
+                            </p>
+                            <p>
+                                Serving Temperature:
+                                {{ $cocktail['serving_temperature'] }}
+                            </p>
+                            <p>
+                                Garnish:
+                                {{ $cocktail['garnish'] }}
+                            </p>
+                            <p>
+                                price:
+                                {{ $cocktail['price'] }}€
+                            </p>
                         </li>
                     @endforeach
                 </ul>
             </div>
-            <div class="row">
+            <div>
+                <div class="drink-img">
+                    <img src="https://static.vecteezy.com/system/resources/previews/019/606/487/original/cocktail-graphic-clipart-design-free-png.png"
+                        alt="drink">
+                </div>
 
             </div>
 

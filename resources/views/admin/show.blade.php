0616afff204be3ca29ffd4118109580477ab9c5c
cosimogiove <cosimo18497@gmail.com>
2024-02-20 16:43:07 +0100

    feat:style

--- resources/views/admin/show.blade.php
+++ resources/views/admin/show.blade.php
@@ -1,44 +1,56 @@
 @extends('layouts.app')
 
 @section('content')
-  <div class="container pt-4 pb-4">
-    <h2 class="text-center">
-      {{ $cocktail['name'] }}
-  </h2>
-  <p>
-      Main Alcohol:
-      {{ $cocktail['main_alcohol'] }}
-  </p>
-  <p>
-      Ingredients:
-      <ul>
-        @foreach ($cocktail->ingredients as $ingredient)
-            <li>
-                {{$ingredient->name}}
-            </li>
-        @endforeach
-      </ul>
-  </p>
-  <p>
-      Preparation:
-      {{ $cocktail['preparations'] }}
-  </p>
-  <p>
-      Glass Type:
-      {{ $cocktail['glass_type'] }}
-  </p>
-  <p>
-      Serving Temperature:
-      {{ $cocktail['serving_temperature'] }}
-  </p>
-  <p>
-      Garnish:
-      {{ $cocktail['garnish'] }}
-  </p>
-  <p>
-      Price:
-      {{ $cocktail['price'] }}€
-  </p>
-
-  </div>
+    <div class="container pt-5 pb-4">
+        <div class="d-flex justify-content-between">
+            <div class="cocktail-img-container">
+                <img src="https://www.transparentpng.com/thumb/cocktail/bTJb4v-cocktail-picture.png" alt="{{$cocktail["name"]}}">
+            </div>
+            <div class="cocktail-deatails-container">
+                <h2 class="pb-3 ps-4">
+                    {{ $cocktail['name'] }}
+                </h2>
+                <p>
+                    <span class="bold">
+                        Main Alcohol:
+                    </span>
+                    
+                    {{ $cocktail['main_alcohol'] }}
+                </p>
+                <p>
+                    <span class="bold">Ingredients:</span>
+                <ul>
+                    @foreach ($cocktail->ingredients as $ingredient)
+                        <li>
+                            {{ $ingredient->name }}
+                        </li>
+                    @endforeach
+                </ul>
+                </p>
+                <p>
+                    <span class="bold">Preparation:</span>
+                    {{ $cocktail['preparations'] }}
+                </p>
+                <p>
+                    <span class="bold">Glass type:</span>
+                    {{ $cocktail['glass_type'] }}
+                </p>
+                <p>
+                    <span class="bold">Serving temperature:</span>
+                    {{ $cocktail['serving_temperature'] }}
+                </p>
+                <p>
+                    <span class="bold">Garnish:</span>
+                    {{ $cocktail['garnish'] }}
+                </p>
+                <p>
+                    <span class="bold">Price:</span>
+                    {{ $cocktail['price'] }}€
+                </p>
+            </div>
+        </div>
+        <div class="pt-3 text-center">
+            <a role="button" href="{{route("cocktail.index")}}" class="btn buttonbg "> Back to Cocktails List</a>
+        </div>
+    </div>
 @endsection
